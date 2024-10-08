42aa8d203f9a10f6360132c176ed01addc685e94
nico9812 <nicocalderon9818@gmail.com>
2024-10-08 06:29:33 -0300

    [add]

--- invitacion.js
+++ invitacion.js
@@ -1,25 +1,47 @@
-var carr = 1;
+
 document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("form");
+    form.addEventListener("submit",function(event){
+        event.preventDefault();
+        const formData = new URLSearchParams();
+        formData.append("entry.851951850",document.getElementById("nombre").value);
+        fetch("https://docs.google.com/forms/d/e/1FAIpQLSekp6A836wLEbD7Jj6WoN_Ntabd64vqzL8X10DCnh11D9d83g/formResponse",{
+            method: "POST",
+            body:formData,
+            headers:{
+                "Content-Type":"application/x-www-form-urlencoded"
+            },
+            mode:"no-cors"
+        }).then(()=>{
+            form.reset()
+            cerrarmodal();
+            abrirmodalok();
+            setTimeout(cerrarmodalok, 2000);
 
-});
-
-function clickC(c){
-    let imagenes = document.querySelectorAll('.image');
-    imagenes.forEach(function(imagen) {
-        imagen.style.zIndex = '-1';
+        }).catch(error =>{
+            const span = document.getElementById("spanerror");
+            span.style.display="block";
+        });
     });
 
-    let valor = Number(c.getAttribute('value'));
-    carr = carr + valor;
-    if (carr>3){
-        carr = 1;
-    }
-    else if(carr < 1){
-        carr = 3;
-    }
-    valor = "image"+carr;
-    let image = document.getElementById(valor);
-    image.style.zIndex = "1";
-    
+});
+function abrirmodalok(){
+    const modal = document.getElementById("modalok");
+    modal.style.display="flex";
+}
+function cerrarmodalok(){
+    const modal = document.getElementById("modalok");
+    modal.style.display="none";
+}
 
-};
+function abrirmodal(){
+    const span = document.getElementById("spanerror");
+    span.style.display="none";
+    const modal = document.getElementById("modalinvitacion");
+    modal.style.display="flex";
+}
+function cerrarmodal(){
+    const modal = document.getElementById("modalinvitacion");
+    modal.style.display="none";
+}
+
